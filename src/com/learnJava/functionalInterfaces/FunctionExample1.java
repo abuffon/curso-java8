81851f2dea6f17f4c55ffa8ece1075a42916ee6e
Anderson Buffon Berto <abuffon@gmail.com>
2022-01-04 21:20:32 -0300

    Predicate and Function examples

---
+++ src/com/learnJava/functionalInterfaces/FunctionExample1.java
@@ -0,0 +1,13 @@
+package com.learnJava.functionalInterfaces;
+
+public class FunctionExample1 {
+
+	public static String performConcat(String str) {
+		return FunctionExample.addSomeString.apply(str);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(performConcat("Hello"));
+	}
+
+}
--- src/com/learnJava/functionalInterfaces/PredicateStudentExample.java
+++ src/com/learnJava/functionalInterfaces/PredicateStudentExample.java
@@ -9,8 +9,10 @@ import com.learnJava.data.StudentDataBase;
 public class PredicateStudentExample {
 
 	static Predicate<Student> p1 = (s) -> s.getGradeLevel() >= 3;
+	static Predicate<Student> p2 = (s) -> s.getGpa() >= 3.9;
 	
 	public static void filterStudentByGradeLevel() {
+		System.out.println("filterStudentByGradeLevel()");
 		List<Student> allStudents = StudentDataBase.getAllStudents();
 		
 		allStudents.forEach((s -> {
@@ -20,8 +22,31 @@ public class PredicateStudentExample {
 		}));
 	}
 	
+	public static void filterStudentByGpa() {
+		System.out.println("filterStudentByGpa()");
+		List<Student> allStudents = StudentDataBase.getAllStudents();
+		
+		allStudents.forEach((s -> {
+			if (p2.test(s)) {
+				System.out.println(s);
+			}
+		}));
+	}
+	
+	public static void filterStudents() {
+		System.out.println("filterStudents()");
+		List<Student> allStudents = StudentDataBase.getAllStudents();
+		
+		allStudents.forEach((s -> {
+			if (p1.or(p2).negate().test(s)) {
+				System.out.println(s);
+			}
+		}));
+	}
+	
 	public static void main(String[] args) {
 		filterStudentByGradeLevel();
-
+		filterStudentByGpa();
+		filterStudents();
 	}
 }
